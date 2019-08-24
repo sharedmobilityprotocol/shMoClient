f89f3a1559efe347882866c1a75462955cf819ff
Troels <tplenge@pm.me>
2019-08-24 20:00:37 +0200

    Updated networks in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -61,8 +61,17 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-     ropsten: {
-       provider: () => new HDWalletProvider(mnemonic, `ropsten.infura.io/v3/c45a171d2044435b89854c364357a13f`),
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `rinkeby.infura.io/v3/c45a171d2044435b89854c364357a13f`),
+      network_id: 2,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    ropsten: {
+       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/c45a171d2044435b89854c364357a13f`),
        network_id: 3,       // Ropsten's id
        gas: 5500000,        // Ropsten has a lower block limit than mainnet
        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
@@ -70,7 +79,16 @@ module.exports = {
        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
      },
 
-    // Useful for private networks
+    gorli: {
+      provider: () => new HDWalletProvider(mnemonic, `goerli.infura.io/v3/c45a171d2044435b89854c364357a13f`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+   // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
       // network_id: 2111,   // This network is yours, in the cloud.
