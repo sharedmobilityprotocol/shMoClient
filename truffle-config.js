74fd5e4ce0383f7e521587ccd24d5a9a182481d0
rasor <soren@rasor.dk>
2019-08-25 09:11:02 +0200

    Skale added

--- truffle-config.js
+++ truffle-config.js
@@ -22,6 +22,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraKey = "fbbc46df8aa64cc3b6eac08863705452";
 
 const fs = require('fs');
+//const infuraKey = fs.readFileSync(".infura").toString().trim();
 const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
@@ -85,6 +86,15 @@ module.exports = {
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    skale: {
+      provider: () => new HDWalletProvider(mnemonic, `https://68.183.165.161:10205`),
+      network_id: 5,       // ???'s id
+      gas: 0,              // 
+      //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
    // Useful for private networks
